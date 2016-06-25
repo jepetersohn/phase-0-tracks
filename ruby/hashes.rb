f20c93041a3c903cb8d0d704a679f6f9aabfe994
Joan Petersohn <jepetersohn@gmail.com>
2016-06-25 13:10:31 -0600

    tweak code

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,44 +1,56 @@
+client_survey = {
+	name: " ", 
+	age: " ",  
+	number_of_children: " ", 
+	decor_theme: " ", 
+	likes_carpeting: " ", 
+	likes_vaulted_ceilings: " ", 
+}
+
 puts "What is the client's full name?"
 
-name = gets.chomp
+client_survey[:name] = gets.chomp.to_s
 
 puts "How old is the client?"
 
-age = gets.chomp
+client_survey[:age] = gets.chomp.to_i
 
 puts "How many children does the client have?"
 
-number_of_children = gets.chomp
+client_survey[:number_of_children] = gets.chomp.to_i
 
 puts "What decor theme would they prefer?"
-decor_theme = gets.chomp 
+client_survey[:decor_theme] = gets.chomp.to_s 
 
 puts "Do they like carpeting? (yes or no)"
 
-likes_carpeting = gets.chomp
+client_survey[:likes_carpeting] = gets.chomp
 
-until likes_carpeting == "yes" || likes_carpeting == "no"
+until client_survey[:likes_carpeting] == "yes" || client_survey[:likes_carpeting] == "no"
 puts "I did not understand your answer. Please enter 'yes' or 'no.'"
-likes_carpeting = gets.chomp
+client_survey[:likes_carpeting] = gets.chomp
 end
 
-if likes_carpeting == "yes"
-	likes_carpeting = true
-elsif likes_carpeting == "no"
-	likes_carpeting = false
+if client_survey[:likes_carpeting] == "yes"
+	client_survey[:likes_carpeting] = true
+elsif client_survey[:likes_carpeting] == "no"
+	client_survey[:likes_carpeting] = false
+end
+
+puts "Do they like vaulted ceilings? (yes or no)"
+
+client_survey[:likes_vaulted_ceilings] = gets.chomp
+
+until client_survey[:likes_vaulted_ceilings] == "yes" || client_survey[:likes_vaulted_ceilings] == "no"
+puts "I did not understand your answer. Please enter 'yes' or 'no.'"
+client_survey[:likes_vaulted_ceilings] = gets.chomp
+end
+
+if client_survey[:likes_vaulted_ceilings] == "yes"
+	client_survey[:likes_vaulted_ceilings] = true
+elsif client_survey[:likes_vaulted_ceilings] == "no"
+	client_survey[:likes_vaulted_ceilings] = false
 end
 
 
-
-client = {
-	name: , 
-	age: ,  
-	number_of_children: , 
-	decor_theme: , 
-	likes_carpeting: , 
-	likes_vaulted_ceilings: , 
-}
-
-
-puts client_survey [:name]
-puts client_survey [:age]
+puts client_survey
