03e969e74c1d5d0bba25e660e49b576f7943ae5b
Joan Petersohn <jepetersohn@gmail.com>
2016-06-27 19:24:52 -0600

    add files

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,35 @@
+# Method to create a list
+# create empty hash
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # break up the string into individual words
+  # make each word a key in the hash
+
+
+
+  # set default quantity
+  # set to default of 1
+  # make the value of each key 1
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+
+# Method to add an item to a list
+# input: item name and optional quantity and list to be added to
+# steps: set the value of the item and quantity as the key-value pair in the hash
+# output: print list and make sure list is returned
+
+# Method to remove an item from the list
+# input: item to be removed and list to be removed from
+# steps: find the item in the hash key and delete that key-value pair from the hash
+# output: list minus the item that was removed
+
+# Method to update the quantity of an item
+# input: change value for a specific key using the new value that it will be changed to
+# steps: find value of key in hash and change it to a new quantity
+# output: list and item with new quantity
+
+# Method to print a list and make it look pretty
+# input: the name of the list that we want to print
+# steps: print out the title of the list first, and then print out the list of items within the hash, followed by their quantity
+# output: the hash with key-value pairs (items and quantities)
