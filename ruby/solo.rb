5715ad8cced60e7af5fcd7ca38669641b9ce49d9
Joan Petersohn <jepetersohn@gmail.com>
2016-07-03 09:57:21 -0600

    add pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,13 @@
+# designing a class preschoolers
+#at least three attributes (using at least two data types) whose values will vary from instance to instance (example: age)
+#attributes
+#age from 3-6
+#gender
+#hair color
+
+#at least three methods, at least one of which takes an argument
+#methods
+#tantrum about something
+#make a mess
+#sleep
+
