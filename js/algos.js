a342d992586cb1ac0f0247e4b8dc176f70bab77d
Joan Petersohn <jepetersohn@gmail.com>
2016-07-18 18:04:06 -0600

    add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+// determine the length of every object within an array 
+// compare the lengths of those objects
+// return the item of the array with the longest length
+
+
+
+// separate the objects of the hash into individual items 
+// compare the items from each hash to see if any of them are the same
+// return true if any of them are the same
+// return false if none of them are the same
+
+
+
+// input an integer (such as 3)
+// generate 1-10 random letters
+// display those random letters together as nonsensical words, corresponding to the number of the integer provided  
+// insert those nonsensical words into an array 
+// print the array 
+// run the function 10 times through the longest-word method above 
+// print the results
